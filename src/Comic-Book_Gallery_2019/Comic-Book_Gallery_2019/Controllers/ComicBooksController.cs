815db05b6bd43e17f35b383acb1807cebfe99333
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2019-11-06 07:26:21 -0800

    Updated Detail Action Method to use ActionResult Types

---
+++ src/Comic-Book_Gallery_2019/Comic-Book_Gallery_2019/Controllers/ComicBooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comic_Book_Gallery_2019.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+           if(DateTime.Today.DayOfWeek== DayOfWeek.Sunday)
+            {
+                return new RedirectResult("/");
+            }
+            return Content( $"Hello again from the controller, ad today is : {DateTime.Today.} ");
+        }
+    }
+}
