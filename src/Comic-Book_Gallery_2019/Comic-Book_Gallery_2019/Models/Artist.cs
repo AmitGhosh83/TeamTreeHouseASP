3eae8ad751562d2fe662a09a0ea2ac8de52b4598
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2019-11-07 08:30:39 -0800

    Added ComicBook and Artist Models

---
+++ src/Comic-Book_Gallery_2019/Comic-Book_Gallery_2019/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic_Book_Gallery_2019.Models
+{
+    public class Artist
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
